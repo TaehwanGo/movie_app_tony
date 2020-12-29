b591d088d5a5254cb0a9c63d243a94f20b40831a
TaehwanGo <gth1123@naver.com>
2020-12-29 19:20:13 +0900

    #4.0 Fetching Movies from API

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios';
 
 class App extends React.Component {
   state = {
@@ -6,9 +7,7 @@ class App extends React.Component {
     movies: [] // 좋은 습관 : 미래에 있을 수도 있는 것에 대한 선언, 여기에 굳이 없어도 되긴함
   }
   componentDidMount(){
-    setTimeout(() => { // delay function
-      this.setState({isLoading:false})
-    }, 6000);
+    axios.get("https://yts-proxy.now.sh/list_movies.json");
   }
   render(){
     const { isLoading } = this.state;
