11d16b9ce5cac14e4504db8317e4fc3dd5d91604
TaehwanGo <gth1123@naver.com>
2020-12-29 15:01:36 +0900

    #3.3 Planning the Movie Component

--- src/App.js
+++ src/App.js
@@ -1,41 +1,20 @@
 import React from 'react';
-// import PropTypes from "prop-types";
 
 class App extends React.Component {
-  constructor(props){
-    super(props);
-    console.log("hello");
-  }
   state = {
-    count:0
-  }
-  add = () => {
-    // this.state.count++; // 이렇게 할 경우 react는 render function을 refresh하지 않음
-    // this.setState({count: this.state.count+1}); // react는 모든것을 다시 render하지 않고 변하는 부분만 새로 업데이트함(virtual DOM에 의해)
-    this.setState(current => ({count: current.count+1})); // setState를 호출 할대마다 react는 새로운 state와 함께 render()를 호출
-  }
-  minus = () => {
-    // this.state.count--; // setState()를 사용해야 됨
-    // this.setState({count: this.state.count-1}); // state에 의존하므로 좋은 코드는 아니기 때문에 나중에 바꿀 예정
-    this.setState(current => ({count: current.count-1}));
+    isLoading: true,
+    movies: [] // 좋은 습관 : 미래에 있을 수도 있는 것에 대한 선언, 여기에 굳이 없어도 되긴함
   }
   componentDidMount(){
-    console.log('componentDidMount');
-  }
-  componentDidUpdate() {
-    console.log('componentDidUpdate');
-  }
-  componentWillUnmount(){
-    console.log('componentWillUnmount');
-    alert('componentWillUnmount');
+    setTimeout(() => { // delay function
+      this.setState({isLoading:false})
+    }, 6000);
   }
   render(){
-    console.log('render');
+    const { isLoading } = this.state;
     return (
       <div>
-        <h1>The number is {this.state.count}</h1>
-        <button onClick={this.add}>Add</button>
-        <button onClick={this.minus}>minus</button>
+        {isLoading ? "Loading..." : "We are ready"}
       </div>
     );
   }
