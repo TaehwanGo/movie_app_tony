281232aecac745d53712654c83d16be2aaaa2342
TaehwanGo <gth1123@naver.com>
2020-12-30 11:16:33 +0900

    #6.1 building the router

---
+++ src/routes/Home.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import axios from 'axios';
+import Movie from '../components/Movie';
+import './Home.css';
+
+class Home extends React.Component {
+  state = {
+    isLoading: true,
+    movies: [], // 좋은 습관 : 미래에 있을 수도 있는 것에 대한 선언, 여기에 굳이 없어도 되긴함
+  };
+
+  getMovies = async () => {
+    const {
+      data: {
+        data: { movies },
+      },
+    } = await axios.get(
+      'https://yts-proxy.now.sh/list_movies.json?sort_by=rating',
+    );
+    // console.log(movies);
+    this.setState({ movies, isLoading: false }); // movies(to state) : movies(from axios) // setState는 render()를 실행함
+  };
+
+  componentDidMount() {
+    this.getMovies();
+  }
+  render() {
+    const { isLoading, movies } = this.state;
+    return (
+      <section className="container">
+        {isLoading ? (
+          <div className="loader">
+            <span className="loader__text">Loading...</span>
+          </div>
+        ) : (
+          <div className="movies">
+            {movies.map((movie) => (
+              <Movie
+                key={movie.id}
+                year={movie.year}
+                rating={movie.rating}
+                title={movie.title}
+                summary={movie.summary}
+                poster={movie.medium_cover_image}
+                genres={movie.genres}
+              />
+            ))}
+          </div>
+        )}
+      </section>
+    );
+  }
+}
+
+export default Home;
